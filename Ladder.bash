c0521fc0ef5e24391340cbe52127448b4a2941eb
Joshua-Hoffman-r <63210650+Joshua-Hoffman-r@users.noreply.github.com>
2020-05-07 16:34:39 -0700

    Fixed

--- Ladder.bash
+++ Ladder.bash
@@ -27,33 +27,21 @@
 
 Ladder () {
 
-Input=$1
+Input=$1 
 cat $1 > .$1.bak
-
 declare -ag Checks=( $(awk '/#MD5/ && !/awk/ {print $2}' .$1.bak | tr -d \#) )
-declare -AG ${Checks[@]}
+declare -Ag ${Checks[@]}
+MD5_All=$(echo ${MD5[@]} | tr ' ' '|')
+Unchecked="$(awk '{gsub(/'${MD5_All}'/, ""); print $0}' .$1.bak)"
 
 	for Steps in ${!MD5[@]}; do
-	declare -Ag Functions[$Steps]="$(awk '/'"$Steps \(\) \{"'/,/'${MD5[$Steps]}'/' .$1.bak )"
+	declare -Ag Functions[$Steps]="$(echo "$Unchecked" | awk '/'"$Steps \(\) \{"'/,/'#MD5[$Steps]'/')"
+	declare -Ag MD5_New[$Steps]="$(echo "${Functions[$Steps]}" | awk '{gsub(/'${MD5[$Steps]}'/, ""); print $0}' | md5sum | cut -c 1-32)"
+	Substring="gsub("/${MD5[$Steps]}"/, \""${MD5_New[$Steps]}"\");$Substring"
 	done
 	
-	if [[ ${Ladder_1:=1} = 1 ]]; then
-	
-		for Verify in ${!Functions[@]}; do
-		declare -Ag MD5_New[$Verify]="$(echo "${Functions[$Verify]}" | awk '{gsub(/'${MD5[$Verify]}'/, ""); print $0}' | md5sum | cut -c 1-32)"
-		done
-		
-	fi
-		
-	for Edit in ${!Functions[@]}; do
-		
-		if [[ ${MD5[$Edit]} != ${MD5_New[$Edit]} ]]; then
-		declare -Ag Functions_New[$Edit]="$(echo "${Functions[$Edit]}" | awk '{gsub(/'${MD5[$Edit]}'/, "'${MD5_New[$Edit]}'"); print $0}')"
-		awk '{gsub(/'${MD5[$Edit]}'/, "'${MD5_New[$Edit]}'"); print $0}' .$1.bak | tee .$1.bak
-		fi
-		
-	done
-	
+awk '{'"$Substring"'print $0}' .$1.bak > $1
+
 }
 
 Ladder $@
